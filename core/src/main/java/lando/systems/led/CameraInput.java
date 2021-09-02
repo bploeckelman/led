aac945346734c99fafc594744846299bdb06e580
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-09-02 01:08:23 -0500

    Add a pan/zoom camera controller

---
+++ core/src/main/java/lando/systems/led/CameraInput.java
@@ -0,0 +1,153 @@
+package lando.systems.led;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class CameraInput extends InputAdapter implements GestureDetector.GestureListener {
+
+    static final float camera_min_x = -2000;
+    static final float camera_max_x =  2000;
+    static final float camera_min_y = -2000;
+    static final float camera_max_y =  2000;
+    static final float zoom_min =  0.05f;
+    static final float zoom_max =  5f;
+    static final float pan_speed = 6f;
+    static final float zoom_speed = 2f;
+    static final float default_zoom = 1f;
+
+    final OrthographicCamera camera;
+    final Vector3 touch_pos;
+    final Vector2 target_pos;
+    float target_zoom;
+    boolean shift_down;
+
+    public final Vector2 effective_viewport;
+    public boolean panning;
+
+    public CameraInput(OrthographicCamera camera) {
+        this.camera = camera;
+        this.touch_pos = new Vector3();
+        this.target_pos = new Vector2();
+        this.target_zoom = default_zoom;
+        this.effective_viewport = new Vector2();
+        this.panning = false;
+        this.shift_down = false;
+        reset_camera();
+    }
+
+    public void reset_camera() {
+        camera.zoom = default_zoom;
+        camera.setToOrtho(false, Config.viewport_width, Config.viewport_height);
+        camera.translate(-Config.viewport_width / 2f, -Config.viewport_height / 2f);
+        camera.update();
+
+        effective_viewport.set(
+                camera.viewportWidth  * camera.zoom,
+                camera.viewportHeight * camera.zoom);
+
+        target_zoom = camera.zoom;
+        target_pos.set(camera.position.x, camera.position.y);
+
+        panning = false;
+        shift_down = false;
+    }
+
+    public void update(float dt) {
+        shift_down = (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)
+                   || Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT));
+
+        camera.position.x = MathUtils.lerp(camera.position.x, target_pos.x, pan_speed * dt);
+        camera.position.y = MathUtils.lerp(camera.position.y, target_pos.y, pan_speed * dt);
+
+        target_zoom = MathUtils.clamp(target_zoom, zoom_min, zoom_max);
+        camera.zoom = Interpolation.exp10Out.apply(camera.zoom, target_zoom, zoom_speed * dt);
+        effective_viewport.set(
+                camera.viewportWidth  * camera.zoom,
+                camera.viewportHeight * camera.zoom);
+
+        camera.position.x = MathUtils.clamp(camera.position.x, camera_min_x + effective_viewport.x / 2f, camera_max_x - effective_viewport.y / 2f);
+        camera.position.y = MathUtils.clamp(camera.position.y, camera_min_y + effective_viewport.x / 2f, camera_max_y - effective_viewport.y / 2f);
+
+        camera.update();
+    }
+
+    // ------------------------------------------------------------------------
+    // GestureListener implementation
+
+    @Override
+    public boolean touchDown(float x, float y, int pointer, int button) {
+        touch_pos.set(x, y, 0);
+        camera.unproject(touch_pos);
+        return true;
+    }
+
+    @Override
+    public boolean tap(float x, float y, int count, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean longPress(float x, float y) {
+        return false;
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        target_pos.set(camera.position.x - deltaX, camera.position.y + deltaY);
+        panning = true;
+        return true;
+    }
+
+    @Override
+    public boolean panStop(float x, float y, int pointer, int button) {
+        panning = false;
+        return false;
+    }
+
+    @Override
+    public boolean zoom(float initialDistance, float distance) {
+        final float slow_factor = (shift_down ? 0.1f : 1f);
+        target_zoom += Math.signum(initialDistance - distance) * slow_factor;
+        return true;
+    }
+
+    @Override
+    public boolean scrolled(float amountX, float amountY) {
+        final float slow_factor = (shift_down ? 0.1f : 1f);
+        target_zoom += Math.signum(amountY) * slow_factor;
+        return true;
+    }
+
+    @Override
+    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+        return false;
+    }
+
+    @Override
+    public void pinchStop() {
+
+    }
+
+//    @Override
+//    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+//        return super.pinch(initialPointer1, initialPointer2, pointer1, pointer2);
+//    }
+//
+//    @Override
+//    public void pinchStop() {
+//        super.pinchStop();
+//    }
+
+}
--- core/src/main/java/lando/systems/led/Main.java
+++ core/src/main/java/lando/systems/led/Main.java
@@ -3,18 +3,19 @@ package lando.systems.led;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.github.xpenatan.imgui.ImGui;
 import com.github.xpenatan.imgui.ImGuiExt;
 import com.github.xpenatan.imgui.ImGuiInt;
 import com.github.xpenatan.imgui.enums.ImGuiConfigFlags;
-import com.github.xpenatan.imgui.enums.ImGuiInputTextFlags;
 import com.github.xpenatan.imgui.gdx.ImGuiGdxImpl;
 import com.github.xpenatan.imgui.gdx.ImGuiGdxInput;
 import space.earlygrey.shapedrawer.ShapeDrawer;
@@ -32,6 +33,7 @@ public class Main extends ApplicationAdapter {
     ImGuiInt grid;
 
     OrthographicCamera camera;
+    CameraInput camera_input;
 
     @Override
     public void create() {
@@ -56,10 +58,15 @@ public class Main extends ApplicationAdapter {
         grid = new ImGuiInt(default_grid);
 
         camera = new OrthographicCamera();
-        camera.setToOrtho(false, Config.viewport_width, Config.viewport_height);
 
+        camera_input = new CameraInput(camera);
         imgui_input = new ImGuiGdxInput();
-        Gdx.input.setInputProcessor(imgui_input);
+        var input_mux = new InputMultiplexer(
+                imgui_input,
+                camera_input,
+                new GestureDetector(camera_input)
+        );
+        Gdx.input.setInputProcessor(input_mux);
     }
 
     @Override
@@ -80,25 +87,32 @@ public class Main extends ApplicationAdapter {
             Gdx.app.exit();
         }
 
+        var dt = Gdx.graphics.getDeltaTime();
+
+        camera_input.update(dt);
+
         imgui.update();
-        camera.update();
     }
 
     @Override
     public void render() {
         update();
 
-        ScreenUtils.clear(Color.DARK_GRAY);
+        ScreenUtils.clear(0.2f, 0.2f, 0.2f, 1f);
 
         // build imgui frame
         ImGui.Begin("Level Editor (LED)");
         {
+            if (ImGui.Button("Reset", 100, 25)) {
+                grid.setValue(default_grid);
+                camera_input.reset_camera();
+            }
+
+            ImGui.Separator();
+
             ImGui.Text("Grid Size");
             ImGui.InputInt("", grid, 4, 16);
             ImGui.SliderInt("", grid, 4, 256);
-            if (ImGui.Button("Reset", 115, 20)) {
-                grid.setValue(default_grid);
-            }
         }
         ImGui.End();
         ImGui.Render();
@@ -107,10 +121,11 @@ public class Main extends ApplicationAdapter {
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         {
+            // draw grid
             var steps = 300;
             var scale = 2000f;
             var width = 1f;
-            var color = Color.LIGHT_GRAY;
+            var color = Color.DARK_GRAY;
             int grid_size = grid.getValue();
             // TODO: clip to only draw range within current camera bounds
             for (int y = -steps; y <= steps; y++) {
@@ -119,8 +134,20 @@ public class Main extends ApplicationAdapter {
                     drawer.line(-scale, y * grid_size, scale, y * grid_size, color, width);
                 }
             }
-            drawer.line(-scale, 0, scale, 0, Color.RED, 4);
-            drawer.line(0, -scale, 0, scale, Color.GREEN, 4);
+
+            if (camera_input.panning) {
+                // draw a dimming overlay
+                batch.setColor(0f, 0f, 0f, 0.2f);
+                batch.draw(pixel,
+                        camera.position.x - camera_input.effective_viewport.x / 2f,
+                        camera.position.y - camera_input.effective_viewport.y / 2f,
+                        camera_input.effective_viewport.x,
+                        camera_input.effective_viewport.y);
+                batch.setColor(Color.WHITE);
+            }
+
+            drawer.line(-scale, 0, scale, 0, Color.FIREBRICK, 2);
+            drawer.line(0, -scale, 0, scale, Color.FOREST, 2);
         }
         batch.end();
 
