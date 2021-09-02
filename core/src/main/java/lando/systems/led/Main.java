2b92dbcc3d93c5c20b8a3710e6cd1962acd589c1
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-09-01 22:46:14 -0500

    Add shapedrawer lib, draw a grid

--- core/src/main/java/lando/systems/led/Main.java
+++ core/src/main/java/lando/systems/led/Main.java
@@ -2,25 +2,46 @@ package lando.systems.led;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.github.xpenatan.imgui.ImGui;
 import com.github.xpenatan.imgui.ImGuiExt;
+import com.github.xpenatan.imgui.ImGuiInt;
 import com.github.xpenatan.imgui.enums.ImGuiConfigFlags;
 import com.github.xpenatan.imgui.gdx.ImGuiGdxImpl;
 import com.github.xpenatan.imgui.gdx.ImGuiGdxInput;
+import space.earlygrey.shapedrawer.ShapeDrawer;
 
 /* https://github.com/xpenatan/jDear-imgui/ */
 public class Main extends ApplicationAdapter {
 
     SpriteBatch batch;
+    ShapeDrawer drawer;
+    Texture pixel;
     ImGuiGdxImpl imgui;
     ImGuiGdxInput imgui_input;
+
+    final int default_grid = 16;
+    ImGuiInt grid;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        drawer = new ShapeDrawer(batch);
+
+        // build a temp 1 pixel texture for testing shapedrawer
+        var pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.drawPixel(0, 0);
+        pixel = new Texture(pixmap);
+        pixmap.dispose();
+
+        drawer.setTextureRegion(new TextureRegion(pixel));
 
         ImGui.init();
         ImGuiExt.init();
@@ -28,6 +49,8 @@ public class Main extends ApplicationAdapter {
         ImGui.GetIO().SetDockingFlags(false, false, false, false);
 
         imgui = new ImGuiGdxImpl();
+        grid = new ImGuiInt(default_grid);
+
         // TODO: add ui controls
 
         imgui_input = new ImGuiGdxInput();
@@ -39,6 +62,7 @@ public class Main extends ApplicationAdapter {
         ImGui.dispose();
         imgui.dispose();
         batch.dispose();
+        pixel.dispose();
     }
 
     @Override
@@ -46,18 +70,50 @@ public class Main extends ApplicationAdapter {
         // ...
     }
 
+    public void update() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
+
+        imgui.update();
+    }
+
     @Override
     public void render() {
-        imgui.update();
+        update();
 
-        ScreenUtils.clear(Color.SKY);
+        ScreenUtils.clear(Color.DARK_GRAY);
 
         // build imgui frame
         ImGui.Begin("Level Editor (LED)");
-        ImGui.Text("beep");
+        {
+            ImGui.Text("beep");
+            ImGui.SliderInt("grid", grid, 4, 256);
+        }
         ImGui.End();
         ImGui.Render();
 
+        // draw scene
+        batch.begin();
+        {
+            var steps = 300;
+            var scale = 2000f;
+            var width = 1f;
+            var color = Color.LIGHT_GRAY;
+            int grid_size = grid.getValue();
+            // TODO: clip to only draw range within current camera bounds
+            for (int y = -steps; y <= steps; y++) {
+                for (int x = -steps; x <= steps; x++) {
+                    drawer.line(x * grid_size, -scale, x * grid_size, scale, color, width);
+                    drawer.line(-scale, y * grid_size, scale, y * grid_size, color, width);
+                }
+            }
+            drawer.line(-scale, 0, scale, 0, Color.RED, 4);
+            drawer.line(0, -scale, 0, scale, Color.GREEN, 4);
+        }
+        batch.end();
+
+        // draw ui
         imgui.render(ImGui.GetDrawData());
     }
 
